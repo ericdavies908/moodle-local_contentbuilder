e92ae6ec40491dcc81734c61a0641b8d8ca698db
ericdavies908 <52963894+ericdavies908@users.noreply.github.com>
2026-03-05 22:06:53 +0000

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
  *   sectionsummary — updates the section summary HTML directly.
  *
  * @package    local_contentbuilder
- * @copyright  2026 University of Glasgow LISU {@link https://www.gla.ac.uk}
+ * @copyright  2026 Eric Davies
  * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
